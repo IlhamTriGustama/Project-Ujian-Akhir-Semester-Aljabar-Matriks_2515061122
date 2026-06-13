bd85ca6e470203c5272ce03aa6744dca9d457e82
IlhamTriGustama <igustama18@gmail.com>
2026-06-14 01:38:18 +0700

    Create matriks.py

---
+++ Library/matriks.py
@@ -0,0 +1,29 @@
+import ilham122 as ilham
+
+A = [
+[1, 2],
+[3, 4]
+]
+
+B = [
+[5, 6],
+[2, 3]
+]
+
+print("---Daftar Matriks---")
+print("\nMatriks A:")
+print(A)
+
+print("\nMatriks B:")
+print(B)
+
+print("\n---Hasil Operasi Matriks---")
+print("\nHasil Penjumlahan:")
+print(ilham.penjumlahan(A, B))
+
+print("\nHasil Pengurangan:")
+print(ilham.pengurangan(A, B))
+
+print("\nHasil Perkalian:")
+print(ilham.perkalian(A, B))
+
